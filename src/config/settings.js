da4c73b1fb326dea7538e35c269791e7c44c0e33
jonpng11 <jonpng11@gmail.com>
2019-08-13 02:57:22 -0400

    refactor: WIP modifying the base config for all AJAX calls

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,17 +1,50 @@
+import axios from 'axios';
+
+const Namespace = (path, headers = []) => ({
+    path,
+    headers
+  });
+
 export class BaseConfiguration {
   constructor({
-    siteUrl = '',
-    apiUrl = '',
-    username = '',
-    password = '',
+    siteUrl = 'http://localhost/wp-json',
+    namespace = '/wp/v2',
+    username = 'admin',
+    password = 'admin',
     connectionKey = '',
     connectionToken = ''
   }) {
     this.siteUrl = siteUrl;
-    this.apiUrl = apiUrl;
+    this.namespace = namespace;
     this.username = username;
     this.password = password;
     this.connectionKey = connectionKey;
     this.connectionToken = connectionToken;
+    this.modules = {};
+    this.headers = {};
+    this._request = axios;
+  }
+  get apiUrl() {
+    return `${this.siteUrl}${this.namespace}`;
+  }
+  // eslint-disable-next-line
+  request(requestUrl) {
+    return this._request;
+  }
+  registerModule(namespace = '/') {
+    if (namespace !== '/') {
+      this.modules[namespace] = Namespace(namespace);
+      return this.apiUrl + namespace;
+    }
+  }
+  registerHeader(headerName = '', value = '', namespaces = []) {
+    if (headerName.length && value.length) {
+      this.headers[headerName] = value;
+      namespaces.forEach(namespace => {
+        if (this.modules[namespace]) {
+          this.modules[namespace].headers.push(headerName);
+        }
+      });
+    }
   }
 }
