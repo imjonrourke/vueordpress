901682a9e9cf72801aaca4d4f6544cb52a5acd49
Unknown <jonpng11@gmail.com>
2019-08-14 12:15:02 -0400

    refactor: Changed Page handling

--- src/models/page/page.js
+++ src/models/page/page.js
@@ -1,19 +1,13 @@
 import Post from '../post/post';
-import { getPost } from '../post/post';
 
 /**
  * @class Page
  * Class for Page. Extends Base Post class.
  * Page is a post type of type "page".
  */
-export class Page extends Post {
+export default class Page extends Post {
   constructor({ type = 'page' }) {
     super();
     this.type = type;
   }
-}
-
-export function getPage(args = {}) {
-  const type = 'page';
-  return getPost({ ...args, type });
 }
---
+++ src/models/page/routes.js
@@ -0,0 +1,63 @@
+import { generateRequestParameters } from '../../utils/requestParser';
+import { BaseConfiguration } from '../../config/settings';
+import { RoutesConfigurator } from '../../config/routes';
+import Page from './page';
+
+export function getPage(config) {
+  return (data = {}) => {
+    const dataToQueries = generateRequestParameters(data, ['type', 'id']);
+    const { type = 'page', id } = data;
+    return config.request.get(`/${type}s/${id}?${dataToQueries}`)
+      .then(response => new Page(response.data))
+      .catch(error => error.data);
+  };
+}
+
+export function getPages(config) {
+  return (data = {}) => {
+    const dataToQueries = generateRequestParameters(data, ['type']);
+    const { type = 'page' } = data;
+    return config.request.get(`/${type}s?${dataToQueries}`)
+      .then(response => new Page(response.data))
+      .catch(error => error.data);
+  };
+}
+
+export function createPage(config) {
+  return (data = {}) => {
+    const { type = 'page' } = data;
+    return config.request.post(`/${type}s`, data)
+      .then(response => new Page(response.data))
+      .catch(error => error.data);
+  };
+}
+
+export function updatePage(config) {
+  return (data = {}) => {
+    const { type = 'page', id } = data;
+    return config.request.post(`/${type}s/${id}`)
+      .then(response => new Page(response.data))
+      .catch(error => error.data);
+  };
+}
+
+export function deletePage(config) {
+  return (id, type = 'page') => {
+    return config.request.delete(`/${type}s/${id}`)
+      .then(() => true)
+      .catch(error => error.data);
+  };
+}
+
+export function PageRoutes(config = new BaseConfiguration()) {
+  return RoutesConfigurator(
+    config,
+    [
+      getPage,
+      getPages,
+      createPage,
+      updatePage,
+      deletePage
+    ]
+  );
+}
